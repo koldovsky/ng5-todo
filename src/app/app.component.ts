a3767ff3e06d94cd24be582c5f1266f2fb49931d
Vyacheslav Koldovskyy <koldovsky@koldovsky.com>
2018-02-06 17:59:18 +0200

    Publish

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+import { Todo } from './todo';
+import { TodoDataService } from './todo-data.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [ TodoDataService ]
 })
 export class AppComponent {
-  title = 'app';
+  newTodo: Todo = new Todo();
+
+  constructor(private todoDataService: TodoDataService) {}
+
+  addTodo() {
+    this.todoDataService.addTodo(this.newTodo);
+    this.newTodo = new Todo();
+  }
+
+  removeTodo(todo) {
+    this.todoDataService.deleteTodoById(todo.id);
+  }
+
+  toggleTodoComplete(todo: Todo) {
+    this.todoDataService.toggleTodoComplete(todo)
+  }
+
+  get todos() {
+    return this.todoDataService.getAllTodos();
+  }
+
 }
